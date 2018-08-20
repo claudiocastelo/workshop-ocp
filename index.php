fb60c44d4bf6c546d9a6866be5fa2dc449d79645
root <root@claudio-castelo-tce.c.scale-demo-179814.internal>
2018-08-20 21:58:23 +0000

    2.0

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Openshift Workshop v1.0</h1> ";
+echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 $output = shell_exec('hostname');
 echo "<pre>$output</pre>";
